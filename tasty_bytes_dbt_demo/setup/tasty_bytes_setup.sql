efa55436ef400b505fcddd2ffb6086320ef8cf63
SYS_04411 <manabe@intage.com>
2025-10-27 11:41:04 +0000

    20251027_push

--- tasty_bytes_dbt_demo/setup/tasty_bytes_setup.sql
+++ tasty_bytes_dbt_demo/setup/tasty_bytes_setup.sql
@@ -1,5 +1,5 @@
+/*
 USE ROLE accountadmin;
-
 CREATE OR REPLACE WAREHOUSE tasty_bytes_dbt_wh
     WAREHOUSE_SIZE = 'small'
     WAREHOUSE_TYPE = 'standard'
@@ -11,11 +11,17 @@ CREATE OR REPLACE WAREHOUSE tasty_bytes_dbt_wh
 USE WAREHOUSE tasty_bytes_dbt_wh;
 
 CREATE DATABASE IF NOT EXISTS tasty_bytes_dbt_db;
-CREATE OR REPLACE SCHEMA tasty_bytes_dbt_db.raw;
 CREATE OR REPLACE SCHEMA tasty_bytes_dbt_db.dev;
 CREATE OR REPLACE SCHEMA tasty_bytes_dbt_db.prod;
+*/
+
+USE ROLE sysadmin;
+USE WAREHOUSE tasty_bytes_dbt_wh;
+USE DATABASE tasty_bytes_dbt_db;
+CREATE OR REPLACE SCHEMA tasty_bytes_dbt_db.raw;
 
 
+USE ROLE accountadmin;
 ALTER SCHEMA tasty_bytes_dbt_db.dev SET LOG_LEVEL = 'INFO';
 ALTER SCHEMA tasty_bytes_dbt_db.dev SET TRACE_LEVEL = 'ALWAYS';
 ALTER SCHEMA tasty_bytes_dbt_db.dev SET METRIC_LEVEL = 'ALL';
@@ -24,6 +30,7 @@ ALTER SCHEMA tasty_bytes_dbt_db.prod SET LOG_LEVEL = 'INFO';
 ALTER SCHEMA tasty_bytes_dbt_db.prod SET TRACE_LEVEL = 'ALWAYS';
 ALTER SCHEMA tasty_bytes_dbt_db.prod SET METRIC_LEVEL = 'ALL';
 
+/*
 CREATE OR REPLACE API INTEGRATION git_integration
   API_PROVIDER = git_https_api
   API_ALLOWED_PREFIXES = ('https://github.com/')
@@ -37,6 +44,12 @@ CREATE OR REPLACE NETWORK RULE tasty_bytes_dbt_db.public.dbt_network_rule
 CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION dbt_access_integration
   ALLOWED_NETWORK_RULES = (tasty_bytes_dbt_db.public.dbt_network_rule)
   ENABLED = true;
+*/
+
+USE ROLE sysadmin;
+USE WAREHOUSE tasty_bytes_dbt_wh;
+USE DATABASE tasty_bytes_dbt_db;
+USE SCHEAMA public;
 
 CREATE OR REPLACE FILE FORMAT tasty_bytes_dbt_db.public.csv_ff 
 type = 'csv';
